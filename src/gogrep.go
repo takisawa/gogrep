0f362d9ffd595e430a5885dd80f6019fe418522a
Hiroaki Takisawa <takisawa@hit-start.jp>
2017-08-11 23:54:33 +0900

    Can simple grep.

--- src/gogrep.go
+++ src/gogrep.go
@@ -1,22 +1,47 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"io"
 	"os"
+	"regexp"
 )
 
+const BUFSIZE = 4096
+
 func main() {
+	var fp *os.File
+	var err error
+
 	if len(os.Args) < 3 {
 		fmt.Println("Invalid arguments")
 		os.Exit(1)
 	}
 
 	regexp_text := os.Args[1]
+	regexp := regexp.MustCompile(regexp_text)
+
 	files := os.Args[2:]
 
-	fmt.Printf("regexp_text: %s\n", regexp_text)
+	for _, file := range files {
+		fp, err = os.Open(file)
 
-	for i, file := range files {
-		fmt.Printf("file: %d: %s\n", i, file)
+		if err != nil {
+			panic(err)
+		}
+
+		defer fp.Close()
+
+		reader := bufio.NewReaderSize(fp, BUFSIZE)
+		for line := ""; err == nil; line, err = reader.ReadString('\n') {
+			if regexp.MatchString(line) {
+				fmt.Print(line)
+			}
+		}
+
+		if err != io.EOF {
+			panic(err)
+		}
 	}
 }
